d9b80351080cf7653d22ea6662c92985000a1d6e
gabby-m056 <144246046+gabby-m056@users.noreply.github.com>
2025-10-15 09:50:05 +0100

    rotate

--- Assets/TankScripting.cs
+++ Assets/TankScripting.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class TankScripting : MonoBehaviour
 {
     public float speed = 50.0f;
+    public float turnSpeed = 50.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,10 +13,20 @@ public class TankScripting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //forwards and back
         float throttle = Input.GetAxis("Vertical");
         float movement = throttle * speed * Time.deltaTime;
 
         Transform t = gameObject.transform;
         t.Translate (0.0f, 0.0f, movement);
+
+        //rotate and side
+        float steering = Input.GetAxis("Horizontal");
+        float turn = steering * turnSpeed * Time.deltaTime;
+        t.Rotate (0.0f, turn, 0.0f);
+
+        
     }
+
+    
 }
